39f5fe6e3ff729f0cd364eda78cd9e0d65ece93e
Alain Khan <alainfkhan@gmail.com>
2026-02-16 02:43:55 +0000

    olistbr: rebuild-db: split cep dataset, commit data

--- static/olistbr/rebuild-db/src/ingest/__main__.py
+++ static/olistbr/rebuild-db/src/ingest/__main__.py
@@ -19,6 +19,10 @@ add_schemas: list[str] = ["core"]
 
 
 def main() -> None:
+    # handle cep files
+    # TODO: split csv to 2, then create sql
+    cep_iz_main()
+
     # add generated sql tables
     dbm = DBManager(driver=driver, server=server, database=db)
     try:
@@ -31,22 +35,25 @@ def main() -> None:
             if "core" not in dbm.list_schemas():
                 print(dbm.create_schema("core"))
 
-            # get dim_date sql file
             gen_sql_path = REBUILD_DB_QUERIES_PATH / "rebuild-db"
-            sql_dim_date_path = gen_sql_path / "create-dim_date.sql"
-            with open(sql_dim_date_path, "r") as f:
-                sql_dim_date = f.read()
 
-            cursor.execute(sql_dim_date)
-            conn.commit()
+            # generate core.dim_date
+            if 'core.dim_date' not in dbm.list_tables():
+                sql_dim_date_path = gen_sql_path / "create-dim_date.sql"
+                with open(sql_dim_date_path, "r") as f:
+                    sql_dim_date = f.read()
+
+                cursor.execute(sql_dim_date)
+                conn.commit()
+
+            # create logistics.cep_iz__AuBmA and logistics.cep_iz__B
+            
+
+            # gen_sql_path / 
 
     except Exception as e:
         print("ingest/__main__.py exeption message")
         print(e)
-
-    # handle cep files
-    # TODO: split csv to 2, then create sql
-    cep_iz_main()
 
 
 if __name__ == "__main__":
--- static/olistbr/rebuild-db/src/ingest/cep_iz.py
+++ static/olistbr/rebuild-db/src/ingest/cep_iz.py
@@ -14,11 +14,25 @@ cleaned data will go:
 
 later:
 the tables in sqlserver will be called:
-    logistics.dim_cep_iz_AuBmA
-    logistics.dim_cep_iz_B
+    logistics.dim_cep_iz__AuBmA
+    logistics.dim_cep_iz__B
 
 A = A u (B - A) - (B - A)
 """
+
+import pandas as pd
+from icecream import ic
+
+from src.utils.paths import EXTERNAL_PATH
+
+# debugs
+overwrite_if_exists = False
+
+
+canon_filename = "TB_CEP_BR_2018.csv"
+canon_filepath = EXTERNAL_PATH / "raw" / canon_filename
+
+dataset_url = "https://data.mendeley.com/datasets/g97jb8fp57/1"
 
 
 def get_data() -> None:
@@ -26,19 +40,62 @@ def get_data() -> None:
 
 
 def main() -> None:
-    print("handle A")
-    # if AuBmA exists
-    #   return
-    # else
-    #   generate AuBma
+    """Creates two tables that are partitions of a table from the dataset url."""
 
-    print("handle B")
-    # if B exists
-    #   return
-    # else
-    #   generate B
+    if not canon_filepath.exists():
+        print(f"File '{canon_filename}' not found in '{canon_filepath.parent}'")
+        print(f"Download the dataset from '{dataset_url}'")
+        print(
+            f"Save the downloaded dataset '{canon_filename}' to '{canon_filepath.parent}'"
+        )
+        return
 
-    # create tables if not exists
+    AuBmA_filename = f"{canon_filename.split('.')[0]}__AuBmA.csv"
+    B_filename = f"{canon_filename.split('.')[0]}__B.csv"
+
+    AuBmA_filepath = EXTERNAL_PATH / "processed" / AuBmA_filename
+    B_filepath = EXTERNAL_PATH / "processed" / B_filename
+
+    if AuBmA_filepath.exists() and B_filepath.exists() and not overwrite_if_exists:
+        return
+
+    df = pd.read_csv(
+        canon_filepath,
+        delimiter=";",
+        dtype=str,
+        encoding="utf_8",
+    )
+
+    # partition
+    idx_repeat_header = df[df["CEP"].str.contains("CEP")].index[0]
+    df_A = df.iloc[:idx_repeat_header, :].copy()
+    df_B = df.iloc[idx_repeat_header + 1 :, :-1].copy().reset_index(drop=True)
+
+    # A\B u B\a
+    # symm_diff = set(df_A["CEP"]) ^ set(df_B["CEP"])
+
+    # on col CEP, A\B
+    # cep_AmB = set(df_A["CEP"]) - set(df_B["CEP"])
+
+    # on col CEP B\A
+    cep_BmA = set(df_B["CEP"]) - set(df_A["CEP"])
+
+    # unique CEPs on table B
+    cep_BmA_list = list(sorted(cep_BmA))
+    df_BmA = df[df["CEP"].isin(cep_BmA_list)]
+
+    # AuBmA means A u (B\A) means A union (B set minus A)
+    df_AuBmA = pd.concat([df_A, df_BmA])
+
+    if not AuBmA_filepath.exists() or overwrite_if_exists:
+        print("creating AuBmA")
+        df_AuBmA.to_csv(AuBmA_filepath, index=False)
+        ic(AuBmA_filepath)
+
+    if not B_filepath.exists() or overwrite_if_exists:
+        print("creating B")
+        df_B.to_csv(B_filepath, index=False)
+        ic(B_filepath)
 
 
 if __name__ == "__main__":
